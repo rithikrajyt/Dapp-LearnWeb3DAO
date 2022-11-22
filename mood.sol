744e77333e91ab9ebf5e5df26c107bd745b0dd0a
Rithik Raj Pandey <rrpandey8055@gmail.com>
2022-11-22 12:11:11 +0530

    Added the Smart Contract

---
+++ mood.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.1;
+
+contract moodDiary{
+    string mood;
+
+    function setMood(string memory _mood) public{
+        mood = _mood;
+    }
+
+    function getMood() public view returns(string memory){
+        return mood;
+    }
+
+}
